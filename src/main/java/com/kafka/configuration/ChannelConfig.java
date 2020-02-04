cd749f409d12e70c049494da2f3bfb31b4938225
Mariia Glazova <35651375+MDAUBEITEMANYA@users.noreply.github.com>
2020-02-04 14:18:50 +0300

    small changes

--- src/main/java/com/kafka/configuration/ChannelConfig.java
+++ src/main/java/com/kafka/configuration/ChannelConfig.java
@@ -1,16 +1,23 @@
 package com.kafka.configuration;
 
 import com.kafka.configuration.producer.Item;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,5 +46,36 @@ public class ChannelConfig {
     @Bean
     public KafkaTemplate<String, Item> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Item> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Item> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Item> consumerFactory() {
+        final JsonDeserializer<Item> jsonDeserializer = new JsonDeserializer<>(Item.class);
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(), new StringDeserializer(), jsonDeserializer
+        );
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>(
+                kafkaProperties.buildConsumerProperties()
+        );
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                "tpd-loggers");
+
+        return props;
     }
 }
